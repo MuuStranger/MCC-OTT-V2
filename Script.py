0ea8d57ee1557fb61b4a656a1bf02bcd182d06aa
AKR <98525798+Akrtechi@users.noreply.github.com>
2022-08-09 14:39:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,12 +4,12 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/MOVIE_MEADIA>Team Eva Maria</a>
+✯  𝐉𝐎𝐈𝐍 ➪ <a href=https://t.me/joinchat/Cl6Byc2RvPlkODA1>𝐌𝐌  𝐁𝐚𝐜𝐤𝐮𝐩</a>
+✯  𝐉𝐎𝐈𝐍 ➪ <a href=https://t.me/MM_ROCKERS>𝐌𝐌  𝐑𝐨𝐜𝐤𝐞𝐫𝐬</a>
+✯  𝐉𝐎𝐈𝐍 ➪ <a href=https://t.me/MM_ARCHIVES>𝐌𝐌  𝐀𝐫𝐜𝐡𝐢𝐯𝐞𝐬</a>
+✯  𝐓𝐞𝐚𝐦 ➪ <a href=https://t.me/Movie_Request_Group6>𝗠𝗼𝘃𝗶𝗲 𝗠𝗲𝗮𝗱𝗶𝗮</a>
+★  𝐉𝐨𝐢𝐧 ★ 𝐒𝐡𝐚𝐫𝐞 ★ 𝐒𝐮𝐩𝐨𝐨𝐫𝐭 ★"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/EvamariaTG/EvaMaria  
